ad6017a7f05ea76cfb49fced2ff51ed27f93fe37
arimmie <arimy981002@likelion.org>
2021-08-06 19:16:46 +0900

    [com]alert, picker 추가 완료

--- cupertino_design/lib/main.dart
+++ cupertino_design/lib/main.dart
@@ -52,22 +52,72 @@ class _CupertinoState extends State<Cupertino> {
                 height: 50,
               ),
               CupertinoButton(
-                  child: Text('ios느낌 버튼'),
+                  child: Text('클릭하면 alert창'),
                   borderRadius: BorderRadius.circular(15.0),
                   padding: EdgeInsets.only(right: 40, left: 40),
                   color: Colors.orange,
-                  onPressed: () {}),
+                  onPressed: () {
+                    setState(() {
+                      _showCupertinoDialog();
+                    });
+                  }),
               SizedBox(
                 height: 50,
               ),
               CupertinoButton(
                 child: Text('쿠퍼티노 Picker'),
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _showCupertinoPicker();
+                  });
+                },
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  _showCupertinoDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text('공지'),
+        content: Text('WEVIVOR 체고다!'),
+        actions: <Widget>[
+          CupertinoDialogAction(child: Text('Cancel')),
+          CupertinoDialogAction(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop(); //다이얼로그(alert창) 닫기
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  _showCupertinoPicker() async {
+    //0부터 9까지 숫자 리스트 생성
+    // final _items = List.generate(10, (i) => i);
+    List<String> _items2 = ['소윤', '수민', '아림', '예진'];
+    var result = _items2[0]; //기본값0
+
+    await showCupertinoModalPopup(
+      context: context,
+      builder: (context) => Container(
+        height: 200.0, //피커의 높이
+        child: CupertinoPicker(
+          children:
+              _items2.map((e) => Text('만만치않은녀석들.$e')).toList(), //리스트 요소들 표시
+          itemExtent: 50.0, //항목 1개 높이
+          onSelectedItemChanged: (int value) {
+            result = _items2[value];
+          },
+        ),
+      ),
+    );
+    print(result);
   }
 }
